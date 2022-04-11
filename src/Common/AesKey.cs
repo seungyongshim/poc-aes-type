916b53757eec1b2d228b5cc4c1565be203ed2656
Chris Shim <seungyongshim@users.noreply.github.com>
2022-04-11 17:18:45 +0900

    수정

--- src/Common/AesKey.cs
+++ src/Common/AesKey.cs
@@ -2,4 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Common;
 
-public readonly record struct AesKey([property: MinLength(32), MaxLength(32)] byte[] Value);
+public readonly record struct AesKey([property: MinLength(32), MaxLength(32)] byte[] Value)
+{
+    public override string ToString() => Convert.ToBase64String(Value);
+}
